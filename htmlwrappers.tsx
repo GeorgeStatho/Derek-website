e7be399c05977a5dbe5ee800fd6ef88028892207
GeorgeStatho <georgestathopou@gmail.com>
2026-02-21 23:13:46 -0500

    added wrappers to its own file for organizational purposes

--- htmlwrappers.tsx
+++ htmlwrappers.tsx
@@ -62,3 +62,38 @@ export function divWrap(
     element
   );
 }
+
+export const innershadowdefs = new HTMLElements("defs", {}, [
+      new HTMLElements(//inner shadow
+        "filter",
+        {
+          id: "innerShadow",
+          x: "-50%",
+          y: "-50%",
+          width: "200%",
+          height: "200%",
+          colorInterpolationFilters: "sRGB",
+        },
+        [
+          new HTMLElements("feOffset", { dx: 0, dy: 2 }).toJSX(),
+          new HTMLElements("feGaussianBlur", { stdDeviation: 3, result: "blur" }).toJSX(),
+          new HTMLElements("feComposite", {
+            in: "blur",
+            in2: "SourceAlpha",
+            operator: "arithmetic",
+            k2: -1,
+            k3: 1,
+            result: "innerShadow",
+          }).toJSX(),
+          new HTMLElements("feColorMatrix", {
+            in: "innerShadow",
+            type: "matrix",
+            values: "0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 0.35 0",
+          }).toJSX(),
+          new HTMLElements("feComposite", { in2: "SourceGraphic", operator: "over" }).toJSX(),
+        ]
+      ).toJSX(),
+      new HTMLElements("clipPath", { id: "clip0" }, [
+        new HTMLElements("rect", { fill: "white", width: 127, height: 114 }).toJSX(),
+      ]).toJSX(),
+    ]);
